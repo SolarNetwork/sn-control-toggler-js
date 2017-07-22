c65b9f03775824c84ba4c6b869e54d7e3f7590fb
Matt Magoffin <matt@solarnetwork.net>
2017-07-22 14:38:20 +1200

    Initial project setup.

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import babel from 'rollup-plugin-babel';
+import includePaths from 'rollup-plugin-includepaths';
+
+const includePathOptions = {
+  include: {},
+  paths: ['src'],
+  external: [],
+  extensions: ['.js']
+};
+
+export default {
+  external: id => {
+    return /solarnetwork-api-core/.test(id);
+  },
+  globals: {
+    'solarnetwork-api-core': 'sn',
+  },
+  plugins: [
+    includePaths(includePathOptions),
+    babel({
+      exclude: 'node_modules/**',
+      babelrc: false,
+      plugins: ['external-helpers'],
+      presets: [
+        ['env', {
+          targets: {
+            browsers: ['last 2 versions'],
+            node: 'current',
+          },
+          modules: false,
+        }]
+      ]
+    })]
+};
---
+++ test/controlToggerTests.js
@@ -0,0 +1,4 @@
+import {test,todo} from 'ava';
+
+todo('Tests!');
+
